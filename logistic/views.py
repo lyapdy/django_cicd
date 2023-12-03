beb01cf945c96835685673bd5372f291541e64a2
lyapdy <lyapdy@gmail.com>
2023-12-03 22:09:30 +0700

    Add ci pipeline

--- logistic/views.py
+++ logistic/views.py
@@ -25,4 +25,4 @@ class StockViewSet(ModelViewSet):
 @api_view(['GET'])
 def sample_view(request):
 
-    return Response('Этот текст был изменён!!!!')
+    return Response('Этот текст был изменён!!!')
